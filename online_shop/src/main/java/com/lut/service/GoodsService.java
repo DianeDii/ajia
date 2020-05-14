3a1f9e829e160521bf4578532921657b1e21f5a6
DianeDii <3463600338@qq.com>
2020-05-14 11:26:32 +0800

    登录后主页显示登录名（V.1.0）

---
+++ online_shop/src/main/java/com/lut/service/GoodsService.java
@@ -0,0 +1,12 @@
+package com.lut.service;
+
+import com.lut.model.TbGoods;
+
+import java.util.List;
+
+public interface GoodsService {
+    //用户方法
+    TbGoods showGoods(String goodsID);
+
+    //后台管理员方法
+}
