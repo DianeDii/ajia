804efc1dabc0f661f6e3ceecb8181a8e4b6a35b8
DianeDii <3463600338@qq.com>
2020-05-22 09:11:30 +0800

    集成Swagger

---
+++ online_shop/src/main/java/com/lut/Swagger2.java
@@ -0,0 +1,42 @@
+package com.lut;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    /**
+     * @Description:swagger2的配置文件，这里可以配置swagger2的一些基本的内容，
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(
+                        RequestHandlerSelectors.basePackage("com.lut.controller"))
+                .paths(PathSelectors.any()).build();
+    }
+
+    /**
+     * @Description: 构建 api文档的信息
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 设置页面标题
+                .title("使用swagger2构建api接口文档")
+                // 设置联系人
+                .contact(new Contact("dianedi", "http://localhost:8888/index", "dianediiiii@gmail.com"))
+                // 描述
+                .description("描述信息")
+                // 定义版本号
+                .version("1.0").build();
+    }
+}
--- online_shop/src/main/java/com/lut/interceptor/UsingStaticInterceptor.java
+++ online_shop/src/main/java/com/lut/interceptor/UsingStaticInterceptor.java
@@ -8,5 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 public class UsingStaticInterceptor extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
     }
 }
